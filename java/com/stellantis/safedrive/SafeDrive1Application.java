1e21c46c558b8c2de6f09beba236e68fa50a4cc5
Krishnappa Guvappa <gkrishna4java@gamil.com>
2023-03-07 12:31:32 +0530

    modified existing file

--- java/com/stellantis/safedrive/SafeDrive1Application.java
+++ java/com/stellantis/safedrive/SafeDrive1Application.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SafeDrive1Application {
-
+// hi
 	public static void main(String[] args) {
 		SpringApplication.run(SafeDrive1Application.class, args);
 	}
